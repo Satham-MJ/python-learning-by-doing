36520d298242221d2e91c39eb3378dc2abd1f4e1
Satham-dev <satham.edu@gmail.com>
2026-02-15 21:37:41 +0530

    Comment and Number learning completed

---
+++ basic.py
@@ -0,0 +1,22 @@
+import random
+# Comments - this is a single line commnet 
+"""
+This is a multiline comment
+I am using this as dummy documentation
+"""
+
+# Basic variables
+name="Satham"
+age=32
+isMarried=True
+print(name)
+print(age)
+print(isMarried)
+print(str(age))
+print(float(age))
+print(type(isMarried))
+
+
+# Number Operations
+# In number we have int, float, complex datatypes
+print(random.randrange(1,10))
